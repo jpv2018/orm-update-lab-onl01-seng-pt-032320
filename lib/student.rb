b05573388f3ff067898a0ca765fc1e9a253755aa
Jason Viola <jason.viola@mavs.uta.edu>
2020-06-09 21:10:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,11 +5,6 @@ class Student
 attr_accessor :name, :grade
 attr_reader :id
 
-  def initialize(id=nil, name, grade)
-    @name = name
-    @grade = grade
-    @id = id
-  end
   
   def self.create_table
    sql = <<-SQL
