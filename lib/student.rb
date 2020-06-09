d7656877dc6d6e069ddc6179cbefd49c2d420a7b
Jason Viola <jason.viola@mavs.uta.edu>
2020-06-09 20:40:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,19 @@
 require_relative "../config/environment.rb"
 
 class Student
+  
+attr_accessor :name, :grade
+attr_reader :id
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  def initialize(id=nil, name, grade)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+  
+  def self.create_table
+    <<-SQL
+    
 
 
 end
