c7d87f6f566185278e8f9e08eb7be42afdd8e685
Jason Viola <jason.viola@mavs.uta.edu>
2020-06-09 20:46:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,7 @@ attr_reader :id
       VALUES (?, ?)
       SQL
       DB[:conn].execute(sql, self.name, self.grade)
-      @id = DB[:conn]
+      @id = DB[:conn].execute("SELECT last_id")
       
 
 end
