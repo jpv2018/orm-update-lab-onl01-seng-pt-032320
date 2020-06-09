98bc45e913153ec3ab1773f8fe9a4d26a59ca3d5
Jason Viola <jason.viola@mavs.uta.edu>
2020-06-09 21:11:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,11 @@ class Student
 attr_accessor :name, :grade
 attr_reader :id
 
+  def initialize(id=nil, name, grade)
+    @name = name
+    @grade = grade
+    @id = id
+  end
   
   def self.create_table
    sql = <<-SQL
@@ -41,10 +46,7 @@ attr_reader :id
   end
   
   def self.new_from_db(row)
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
+    new_student = self.new(row[0], row[1], row[2])
     new_student
   end
     
