6e8955a2c47ae3ea7b7df9311e855b1b1e06246a
Jason Viola <jason.viola@mavs.uta.edu>
2020-06-09 20:54:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,12 +45,20 @@ attr_reader :id
     self.new.save
   end
   
-  self.new_from_db(row)
-  new_student = self.new
-  new_student.id = row[0]
-  new_student.name = row[1]
-  new_student.grade = row[2]
+  def self.new_from_db(row)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end
     
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE name = ?
+    SQL
+    DB[:conn].execute(sql)
       
 
 end
